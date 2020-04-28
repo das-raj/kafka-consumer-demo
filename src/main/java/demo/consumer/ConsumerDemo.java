80d4542909af7605b1ff44ace8fa3ba99625123c
Raj Chandvaniya <rajchandvaniya@gmail.com>
2020-04-28 17:50:31 +0530

    Initial Commit:

---
+++ src/main/java/demo/consumer/ConsumerDemo.java
@@ -0,0 +1,40 @@
+package demo.consumer;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092,localhost:9093");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("group.id", "test-group");
+
+        KafkaConsumer myConsumer = new KafkaConsumer(properties);
+
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("test");
+
+        myConsumer.subscribe(topics);
+
+        try{
+            while(true){
+                ConsumerRecords<String, String> records = myConsumer.poll(10);
+                for(ConsumerRecord<String, String> record:records)
+                    System.out.println(String.format("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s",
+                            record.topic(), record.partition(), record.offset(), record.key(), record.value()));
+
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            myConsumer.close();
+        }
+    }
+}
